ec0725d04261062a2162fb60b0ad5248e33560b5
Frank Palomino <frank.palomino@gmail.com>
2021-05-05 20:18:05 +0200

    Login con llamada a api versión 1

--- Proyecto/Frontend/app-react/src/routes/Routes.js
+++ Proyecto/Frontend/app-react/src/routes/Routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from '../pages/Login';
+import Principal from '../pages/Principal';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Login}/>
+        <Route exact path="/principal" component={Principal}/>
       </Switch>
     </BrowserRouter>
   );
