e90383bb66f9f322489149cccdfc6741d2360b4d
KevinrodriguezFernand <krodrigu19@jaumebalmes.net>
2021-05-05 17:21:02 +0200

    adding content & sidebar

---
+++ Proyecto/Frontend/app-react/src/components/content.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class content extends Component {
+    static propTypes = {
+
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default content
