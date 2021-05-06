ddd8d9f67914cc93fcadaa0b49eef03db44372a1
KevinrodriguezFernand <krodrigu19@jaumebalmes.net>
2021-05-06 19:42:19 +0200

    Añadir boton logout

--- Proyecto/Frontend/app-react/src/pages/main.js
+++ Proyecto/Frontend/app-react/src/pages/main.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import '../css/main.css'
 import Sidebar from '../components/sidebar';
-import Home from '../components/Home';
+import Recetas from '../components/Recetas';
 import {Link,Switch, Route } from "react-router-dom";
 import { BrowserRouter as Router } from 'react-router-dom';
+
 
 function Main() {
     return (
@@ -14,16 +15,18 @@ function Main() {
         
         <Sidebar />         
       </div>
-      <div>
-        
+      <div>        
         <Switch>
           <Route path="/home" exact>
-            <Home />
+            <Recetas />
           </Route>
-        </Switch>
-        
+        </Switch>        
       </div>
-      
+      <div>
+        <button className="buttonLogout" onclick="">
+          Logout
+        </button>
+      </div>
     </div>
     </Router>
     );
