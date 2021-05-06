33c88e3d9408cfd29e6a6e18aeb1bc2eb81bb4e9
Frank Palomino <frank.palomino@gmail.com>
2021-05-06 20:49:49 +0200

    Circuito completado

--- Proyecto/Frontend/app-react/src/pages/main.js
+++ Proyecto/Frontend/app-react/src/pages/main.js
@@ -1,35 +1,50 @@
-import React, { Component } from 'react'
-import '../css/main.css'
-import Sidebar from '../components/sidebar';
-import Recetas from '../components/Recetas';
-import {Link,Switch, Route } from "react-router-dom";
-import { BrowserRouter as Router } from 'react-router-dom';
+import React, { Component } from "react";
+import "../css/main.css";
+import Sidebar from "../components/sidebar";
+import Recetas from "../components/Recetas";
+import { Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import Cookie from "universal-cookie";
 
+const cookie = new Cookie();
 
-function Main() {
+export default class Main extends Component {
+  
+  cerrarSesion = () => {
+    cookie.remove("user", { path: "/" });
+    cookie.remove("username", { path: "/" });
+
+    window.location.href = "./";
+  };
+
+  componentDidMount() {
+    if(!cookie.get('user')){
+      window.location.href ="./";
+    }
+  }
+
+  render(){
     return (
       <Router>
-        <div className='container'>
-      
-      <div className='sideBar'>
-        
-        <Sidebar />         
-      </div>
-      <div>        
-        <Switch>
-          <Route path="/home" exact>
-            <Recetas />
-          </Route>
-        </Switch>        
-      </div>
-      <div>
-        <button className="buttonLogout" onclick="">
-          Logout
-        </button>
-      </div>
-    </div>
-    </Router>
-    );
+        <div className="container">
+          <div className="sideBar">
+            <Sidebar />
+          </div>
+          <div>
+            <Switch>
+              <Route path="/home" exact>
+                <Recetas />
+              </Route>
+            </Switch>
+          </div>
+          <div>
+            <button className="buttonLogout" onClick={()=>this.cerrarSesion()}>
+              Cerrar Sesión
+            </button>
+          </div>
+        </div>
+      </Router>
+    )
   }
   
-  export default Main;
+}
