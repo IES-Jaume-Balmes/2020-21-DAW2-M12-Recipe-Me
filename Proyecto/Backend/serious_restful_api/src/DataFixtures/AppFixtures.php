60b2e88a32e9dc89cb102f56b79873aada11cc0c
Sergi <francessergi@hotmail.com>
2021-05-06 19:40:00 +0200

    añado recetas de testeo

--- Proyecto/Backend/serious_restful_api/src/DataFixtures/AppFixtures.php
+++ Proyecto/Backend/serious_restful_api/src/DataFixtures/AppFixtures.php
@@ -4,8 +4,6 @@ namespace App\DataFixtures;
 
 use Faker\Factory;
 use App\Entity\User;
-use App\Entity\Recipe;
-use App\Entity\Ingredient;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 require_once 'vendor/autoload.php';
@@ -16,14 +14,7 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        // CARGAMOS INGREDIENTES
-        for ($i = 0; $i < 20; $i++) {
-            $ingrediente = new Ingredient();
-            $ingrediente->setName("Ingrediente $i");
-            $manager->persist($ingrediente);
-        }
-
-        //NOS CARGAMOS
+        //CARGAMOS ADMINS
         $administradores = [
             'frank' => '26253c50741faa9c2e2b836773c69fe6',
             'kevin' => '9d5e3ecdeb4cdb7acfd63075ae046672',
@@ -34,6 +25,7 @@ class AppFixtures extends Fixture
             $usuario->setUsername($admin);
             $usuario->setEmail("$admin@recipeme.com");
             $usuario->setPassword($password);
+            $usuario->setRoles(['ROLE_ADMIN']);
             $manager->persist($usuario);
         }
 
@@ -46,18 +38,6 @@ class AppFixtures extends Fixture
             $usuario->setPassword($this->faker->password);
             $manager->persist($usuario);
         }
-
-        // CARGAMOS RECETAS
-        /* for ($i = 0; $i < 20; $i++) {
-            $receta = new Recipe();
-            $ingredient = new Ingredient();
-            $ingredient->setName('pasta');
-
-            $receta->setDescription('Descripcion random');
-            $receta->setName('Nombre de la receta');
-            $receta->addIngredient($ingredient);
-            $manager->persist($receta);
-        } */
 
         $manager->flush();
     }
