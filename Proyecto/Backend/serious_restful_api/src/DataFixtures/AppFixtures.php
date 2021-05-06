c1575e84e0c42bf7e3f4a4b383d17d38985c8479
Sergi <francessergi@hotmail.com>
2021-05-06 16:27:33 +0200

    fixtures

--- Proyecto/Backend/serious_restful_api/src/DataFixtures/AppFixtures.php
+++ Proyecto/Backend/serious_restful_api/src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Ingredient;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -11,6 +12,11 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+        for ($i = 0; $i < 20; $i++) {
+            $ingrediente = new Ingredient();
+            $ingrediente->setName("Ingrediente $i");
+            $manager->persist($ingrediente);
+        }
 
         $manager->flush();
     }
