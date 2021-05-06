20cb76b053b095fa9ec06eb29ad7af39c88d088f
Sergi <francessergi@hotmail.com>
2021-05-06 17:07:23 +0200

    user fixture

--- Proyecto/Backend/serious_restful_api/src/DataFixtures/AppFixtures.php
+++ Proyecto/Backend/serious_restful_api/src/DataFixtures/AppFixtures.php
@@ -2,21 +2,43 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
+use App\Entity\Recipe;
 use App\Entity\Ingredient;
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // CARGAMOS INGREDIENTES
         for ($i = 0; $i < 20; $i++) {
             $ingrediente = new Ingredient();
             $ingrediente->setName("Ingrediente $i");
             $manager->persist($ingrediente);
         }
+
+        // CARGAMOS USUARIOS
+        for ($i = 0; $i < 20; $i++) {
+            $usuario = new User();
+            $usuario->setUsername("Usuario $i");
+            $usuario->setEmail("usuario$i@recipeme.com");
+            $usuario->setPassword('123456');
+            $manager->persist($usuario);
+        }
+
+        // CARGAMOS RECETAS
+        /* for ($i = 0; $i < 20; $i++) {
+            $receta = new Recipe();
+            $ingredient = new Ingredient();
+            $ingredient->setName('pasta');
+
+            $receta->setDescription('Descripcion random');
+            $receta->setName('Nombre de la receta');
+            $receta->addIngredient($ingredient);
+            $manager->persist($receta);
+        } */
 
         $manager->flush();
     }
