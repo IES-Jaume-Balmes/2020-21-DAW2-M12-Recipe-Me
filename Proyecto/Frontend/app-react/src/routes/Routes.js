4408b223a478e06e5600ea66a7901b55a664d20f
Sergi <francessergi@hotmail.com>
2021-05-12 18:53:11 +0200

    settup base para trabajar con  SignUpSide en Frontend

--- Proyecto/Frontend/app-react/src/routes/Routes.js
+++ Proyecto/Frontend/app-react/src/routes/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "../pages/Login";
 import SignInSide from "../pages/SignInSide";
+import SignUpSide from "../pages/SignUpSide";
 import Main from "../pages/main";
 import Register from "../pages/Register";
 
@@ -11,7 +12,7 @@ function Routes() {
       <Switch>
         {/* <Route exact path="/" component={Login}/> */}
         <Route exact path="/" component={SignInSide} />
-        <Route exact path="/register" component={Register} />
+        <Route exact path="/register" component={SignUpSide} />
         <Route exact path="/main" component={Main} />
       </Switch>
     </BrowserRouter>
