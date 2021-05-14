ced88d34b594fbd0b8e518b77d9097e531774a22
Sergi <francessergi@hotmail.com>
2021-05-14 19:47:59 +0200

    correccion errores padding vista recetas

--- Proyecto/Frontend/app-react/src/components/Cards/RecipeCard.js
+++ Proyecto/Frontend/app-react/src/components/Cards/RecipeCard.js
@@ -42,15 +42,13 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function RecipeReviewCard({ recetas }) {
+export default function RecipeReviewCard({ receta }) {
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
-
-  
 
   const anadirLista = (lista) => {
     const cookie = new Cookie();
@@ -61,118 +59,111 @@ export default function RecipeReviewCard({ recetas }) {
 
     lista.forEach((i) => {
       ingres.push({
-        id : i["@id"],
-        nombre : i.name
-      })
-      
+        id: i["@id"],
+        nombre: i.name,
+      });
     });
 
     let noRepes = [];
 
-    let otro = ingres.filter((obj)=>{
-      if(noRepes.includes(obj.id)){
+    let otro = ingres.filter((obj) => {
+      if (noRepes.includes(obj.id)) {
         return false;
-        
       }
       noRepes.push(obj.id);
       return true;
-    })
+    });
 
     cookie.set("ingredientes", otro, { path: "/" });
     console.log(cookie.get("ingredientes"));
   };
 
   return (
-    <>
-      {recetas.map((receta) => (
-        <Grid item xs={12} sm={6} md={3}>
-          <Card key={receta["@id"]} className={classes.root}>
-            <CardHeader
-              avatar={
-                <Avatar aria-label="recipe" className={classes.avatar}>
-                  R
-                </Avatar>
-              }
-              action={
-                <IconButton aria-label="settings">
-                  <MoreVertIcon />
-                </IconButton>
-              }
-              title={receta.name}
-              subheader="September 14, 2016"
-            />
-            <CardMedia
-              className={classes.media}
-              image="https://source.unsplash.com/collection/1353633"
-              title={receta.name}
-            />
-            <CardContent>
-              <Typography variant="body2" color="textSecondary" component="p">
-                {receta.description}
-              </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-              <IconButton
-                aria-label="add to Lista"
-                onClick={() => {
-                  anadirLista(receta.ingredients);
-                }}
-              >
-                <AddShoppingCartIcon />
-              </IconButton>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-              {/* <IconButton aria-label="share">
+    <Grid item xs={12} sm={6} md={3}>
+      <Card key={receta["@id"]} className={classes.root}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="recipe" className={classes.avatar}>
+              R
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title={receta.name}
+          subheader="September 14, 2016"
+        />
+        <CardMedia
+          className={classes.media}
+          image="https://source.unsplash.com/collection/1353633"
+          title={receta.name}
+        />
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {receta.description}
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton
+            aria-label="add to Lista"
+            onClick={() => {
+              anadirLista(receta.ingredients);
+            }}
+          >
+            <AddShoppingCartIcon />
+          </IconButton>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          {/* <IconButton aria-label="share">
                 <ShareIcon />
               </IconButton> */}
-              <IconButton
-                className={clsx(classes.expand, {
-                  [classes.expandOpen]: expanded,
-                })}
-                onClick={handleExpandClick}
-                aria-expanded={expanded}
-                aria-label="show more"
-              >
-                <ExpandMoreIcon />
-              </IconButton>
-            </CardActions>
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
-              <CardContent>
-                <Typography paragraph>Method:</Typography>
-                <Typography paragraph>
-                  Heat 1/2 cup of the broth in a pot until simmering, add
-                  saffron and set aside for 10 minutes.
-                </Typography>
-                <Typography paragraph>
-                  Heat oil in a (14- to 16-inch) paella pan or a large, deep
-                  skillet over medium-high heat. Add chicken, shrimp and
-                  chorizo, and cook, stirring occasionally until lightly
-                  browned, 6 to 8 minutes. Transfer shrimp to a large plate and
-                  set aside, leaving chicken and chorizo in the pan. Add
-                  pimentón, bay leaves, garlic, tomatoes, onion, salt and
-                  pepper, and cook, stirring often until thickened and fragrant,
-                  about 10 minutes. Add saffron broth and remaining 4 1/2 cups
-                  chicken broth; bring to a boil.
-                </Typography>
-                <Typography paragraph>
-                  Add rice and stir very gently to distribute. Top with
-                  artichokes and peppers, and cook without stirring, until most
-                  of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-                  medium-low, add reserved shrimp and mussels, tucking them down
-                  into the rice, and cook again without stirring, until mussels
-                  have opened and rice is just tender, 5 to 7 minutes more.
-                  (Discard any mussels that don’t open.)
-                </Typography>
-                <Typography>
-                  Set aside off of the heat to let rest for 10 minutes, and then
-                  serve.
-                </Typography>
-              </CardContent>
-            </Collapse>
-          </Card>
-        </Grid>
-      ))}
-    </>
+          <IconButton
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: expanded,
+            })}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>Method:</Typography>
+            <Typography paragraph>
+              Heat 1/2 cup of the broth in a pot until simmering, add saffron
+              and set aside for 10 minutes.
+            </Typography>
+            <Typography paragraph>
+              Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
+              over medium-high heat. Add chicken, shrimp and chorizo, and cook,
+              stirring occasionally until lightly browned, 6 to 8 minutes.
+              Transfer shrimp to a large plate and set aside, leaving chicken
+              and chorizo in the pan. Add pimentón, bay leaves, garlic,
+              tomatoes, onion, salt and pepper, and cook, stirring often until
+              thickened and fragrant, about 10 minutes. Add saffron broth and
+              remaining 4 1/2 cups chicken broth; bring to a boil.
+            </Typography>
+            <Typography paragraph>
+              Add rice and stir very gently to distribute. Top with artichokes
+              and peppers, and cook without stirring, until most of the liquid
+              is absorbed, 15 to 18 minutes. Reduce heat to medium-low, add
+              reserved shrimp and mussels, tucking them down into the rice, and
+              cook again without stirring, until mussels have opened and rice is
+              just tender, 5 to 7 minutes more. (Discard any mussels that don’t
+              open.)
+            </Typography>
+            <Typography>
+              Set aside off of the heat to let rest for 10 minutes, and then
+              serve.
+            </Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+    </Grid>
   );
 }
