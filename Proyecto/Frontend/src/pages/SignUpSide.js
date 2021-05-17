c617ea7e8561581b21023ed382c00d2b550e6752
FrankPalomino <frank.palomino1@gmail.com>
2021-05-17 17:27:22 +0200

    Eliminado codificación md5

--- Proyecto/Frontend/src/pages/SignUpSide.js
+++ Proyecto/Frontend/src/pages/SignUpSide.js
@@ -3,8 +3,6 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Paper from "@material-ui/core/Paper";
 import Box from "@material-ui/core/Box";
@@ -12,13 +10,9 @@ import Grid from "@material-ui/core/Grid";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-
-import md5 from "md5";
 import axios from "axios";
-import Cookie from "universal-cookie";
 
 const baseUrl = "https://localhost:8000/users";
-const cookie = new Cookie();
 
 function Copyright() {
   return (
@@ -82,12 +76,11 @@ export default function SignInSide() {
   }
 
   async function peticionApi() {
-    const axios = require("axios");
 
     let jsonPeticion = {
       email: email,
       username: username,
-      password: md5(password),
+      password: password,
     };
     console.log(jsonPeticion);
 
