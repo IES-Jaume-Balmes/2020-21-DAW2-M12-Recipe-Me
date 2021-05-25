cbe7e74329dce49b1830f9421a0e828ec862a511
KevinrodriguezFernand <krodrigu19@jaumebalmes.net>
2021-05-25 16:34:09 +0200

    Arreglado ya te puedes dar de baja

--- Proyecto/Frontend/src/components/Usuario.js
+++ Proyecto/Frontend/src/components/Usuario.js
@@ -11,32 +11,9 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import axios from "axios";
-import CircleLoader from "react-spinners/CircleLoader";
 
 const cookie = new Cookie();
 const token = cookie.get("token");
-
-/* const styles = (theme) => ({
-  root: {
-    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-    border: 0,
-    borderRadius: 3,
-    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-    color: "white",
-    height: 48,
-    padding: "0 30px",
-  },
-}); */
-
-/*const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };*/
 
 export default class Usuario extends Component {
   handleClickOpen = () => {
@@ -53,17 +30,19 @@ export default class Usuario extends Component {
     const deleteUsuario = `https://127.0.0.1:8000/api/users/${tokenDecoded.userId}`;
     console.log(deleteUsuario);
     await axios
-      .delete(deleteUsuario)
+      .delete(deleteUsuario,{
+        headers:{
+          Authorization: "Bearer " + token,
+        },
+      })
       .then((response) => {
         console.log(response.data);
+        this.cerrarSesion();
       })
       .catch((error) => {
         console.log(error);
         alert("Ha ocurrido un error");
       });
-    cookie.remove("token", { path: "/" });
-    cookie.remove("ingredientes", { path: "/" });
-    window.location.href = "./login";
   };
 
   state = {
@@ -101,8 +80,7 @@ export default class Usuario extends Component {
   }
 
   render() {
-    //const { classes } = this.props;
-    if (this.state.isLoading) return <CircleLoader color={this.state.color} />;
+    
 
     return (
       <>
